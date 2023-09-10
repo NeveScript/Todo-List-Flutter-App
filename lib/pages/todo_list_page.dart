db2f623541ec0c09d894a5667cb4967197c9c7da
NekoYasha <autornekoyasha@gmail.com>
2023-09-10 13:56:35 -0300

    Add text field

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,6 +5,19 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.black,);
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: "Enter the task",
+              hintText: "Try typing: Feed the dog...",
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
