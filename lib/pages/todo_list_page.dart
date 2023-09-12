81ba31c52c9135a2d8b928bbdbe223ccd7a3f5e7
NekoYasha <autornekoyasha@gmail.com>
2023-09-12 11:14:20 -0300

    Add 'pending' row

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -9,28 +9,53 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Add a Task",
-                    hintText: "Example: Buy some winter clothes",
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Add a Task",
+                        hintText: "Example: Buy some winter clothes",
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 8.0,),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.cyan,
+                      padding: const EdgeInsets.all(14.0),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.cyan,
-                  padding: const EdgeInsets.all(14),
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
-                ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: const Text(
+                      "You have 0 pending tasks",
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.cyan,
+                      padding: const EdgeInsets.all(14.0),
+                    ),
+                    child: const Text("Clean all"),
+                  ),
+                ],
               ),
             ],
           ),
