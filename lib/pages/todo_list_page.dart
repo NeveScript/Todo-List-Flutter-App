48531ef49643431a4e2634c7e76f345227e0e4e5
NekoYasha <autornekoyasha@gmail.com>
2023-09-14 12:03:08 -0300

    Add 'taskName' param in the 'TodoListItem' constructor

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -15,82 +15,84 @@ class _TodoListPageState extends State<TodoListPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: taskController,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Add a Task",
-                        hintText: "Example: Buy some winter clothes",
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: taskController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Add a Task",
+                          hintText: "Example: Buy some winter clothes",
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 8.0,),
-                  ElevatedButton(
-                    onPressed: () {
-                      String task = taskController.text;
+                    const SizedBox(width: 8.0,),
+                    ElevatedButton(
+                      onPressed: () {
+                        String task = taskController.text;
 
-                      setState(() {
-                        tasks.add(task);
-                      });
+                        setState(() {
+                          tasks.add(task);
+                        });
 
-                      taskController.clear();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.cyan,
-                      padding: const EdgeInsets.all(14.0),
+                        taskController.clear();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.cyan,
+                        padding: const EdgeInsets.all(14.0),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        size: 30,
+                      ),
                     ),
-                    child: const Icon(
-                      Icons.add,
-                      size: 30,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              Flexible(
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-
-                    for(String task in tasks)
-                      TodoListItem(),
-
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      "You have ${tasks.length} pending tasks",
-                    ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Flexible(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+
+                      for(String task in tasks)
+                        TodoListItem(taskName: task),
+
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.cyan,
-                      padding: const EdgeInsets.all(14.0),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "You have ${tasks.length} pending tasks",
+                      ),
                     ),
-                    child: const Text("Clean all"),
-                  ),
-                ],
-              ),
-            ],
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.cyan,
+                        padding: const EdgeInsets.all(14.0),
+                      ),
+                      child: const Text("Clean all"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
