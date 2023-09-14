de0a297efc277a84dda44cf24066c421dfd90773
NekoYasha <autornekoyasha@gmail.com>
2023-09-14 12:02:41 -0300

    Edit widget style

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,15 +1,18 @@
 import "package:flutter/material.dart";
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({super.key});
+  const TodoListItem({super.key, required this.taskName});
+
+  final String taskName;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4),
-        color: Colors.grey,
+        color: const Color(0xfff3f3f3ff),
       ),
+      margin: const EdgeInsets.all(2.0),
       padding: const EdgeInsets.all(8.0),
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Text(
@@ -18,9 +21,9 @@ class TodoListItem extends StatelessWidget {
             fontSize: 12,
           ),
         ),
-        const Text(
-          "Task",
-          style: TextStyle(
+        Text(
+          taskName,
+          style: const TextStyle(
             fontSize: 16.0,
             fontWeight: FontWeight.w600,
           ),
